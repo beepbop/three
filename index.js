87a3734d26d54ac5e7f749a9fb4bf370cf4c29c2
Brian Woodward <brian.woodward@gmail.com>
2015-08-06 02:14:01 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * three <https://github.com/beepbop/three>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
